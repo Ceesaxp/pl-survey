951869798ec95b289c46029a72f1e368380e1dba
ceesaxp <ceesaxp@905c7a70-3049-0410-9142-0d7f961d69fd>
2008-03-24 15:50:29 +0000

    Minor tweaks to publish script

--- publish.sh
+++ publish.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
 case $1 in
-	local*)
-		DST_ROOT=/srv/www
-		DST_CGI=$DST_ROOT/cgi-bin
-		DST_CSS=$DST_ROOT/htdocs/css
-		DST_DATA=$DST_ROOT/cgi-bin/cgi-data
-		;;
-	remote*)
-		DST_ROOT=//vkocmedb601/citiweb\$
-		DST_CGI=$DST_ROOT/cgi-bin
-		DST_CSS=$DST_ROOT/www/css
-		DST_DATA=$DST_CGI/cgi-data
-		;;
+    local*)
+	DST_ROOT=/srv/www
+	DST_CGI=$DST_ROOT/cgi-bin
+	DST_CSS=$DST_ROOT/htdocs/css
+	DST_DATA=$DST_ROOT/cgi-bin/cgi-data
+	;;
+    remote*)
+	DST_ROOT=//vkocmedb601/citiweb\$
+	DST_CGI=$DST_ROOT/cgi-bin
+	DST_CSS=$DST_ROOT/www/css
+	DST_DATA=$DST_CGI/cgi-data
+	;;
+    *)
+        echo -e "Usage:\n\tpublish.sh {local|remote}\n"
+        exit
+        ;;
 esac
 
 cp surveys.cgi $DST_CGI
