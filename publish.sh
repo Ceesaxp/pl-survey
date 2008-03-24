994c8af8a02a20d5275ff9511cc25ecd934952db
ceesaxp <ceesaxp@905c7a70-3049-0410-9142-0d7f961d69fd>
2008-03-24 15:46:25 +0000

    A few more pieces that were left out in initial commit + build/publish script

---
+++ publish.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+case $1 in
+	local*)
+		DST_ROOT=/srv/www
+		DST_CGI=$DST_ROOT/cgi-bin
+		DST_CSS=$DST_ROOT/htdocs/css
+		DST_DATA=$DST_ROOT/cgi-bin/cgi-data
+		;;
+	remote*)
+		DST_ROOT=//vkocmedb601/citiweb\$
+		DST_CGI=$DST_ROOT/cgi-bin
+		DST_CSS=$DST_ROOT/www/css
+		DST_DATA=$DST_CGI/cgi-data
+		;;
+esac
+
+cp surveys.cgi $DST_CGI
+cp cgi-data/surveys/*.xml $DST_DATA
+cp css/surveys.css $DST_CSS
+
